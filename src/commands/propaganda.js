795557e2f1a3f5e3a1139274f517af1a6fd281f3
Miles <miles.b.flo@gmail.com>
2018-06-14 01:04:43 -0700

    Updated propaganda command to use new file syntax

--- src/commands/propaganda.js
+++ src/commands/propaganda.js
@@ -5,12 +5,12 @@ const path = require('path')
 module.exports = {
   description: 'Display a piece of BoS propaganda.',
   process: async msg => {
-    const imagesPath = '../../public/images/propaganda'
+    const imagesPath = path.join(__dirname, '../../public/images/propaganda')
     fs.readdir(imagesPath, (err, files) => {
       if (err) return msg.channel.send('No assets found.')
       let validFiles = files.filter(file => !file.startsWith('.'))
       let filepath = path.join(imagesPath, sample(validFiles))
-      return msg.channel.sendFile(filepath)
+      return msg.channel.send({ files: [filepath] })
     })
   }
 }
