a318d2d37a13b91f59fe31b3a388bdd3bebb5c1c
Miles <miles.b.flo@gmail.com>
2018-06-15 11:20:44 -0700

    added higher quality image

--- src/commands/map.js
+++ src/commands/map.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = {
   description: 'Display the latest map we have for FO76.',
   process: async msg => {
-    const mapImagePath = path.join(__dirname, '../../public/images/wvmap.png')
+    const mapImagePath = path.join(__dirname, '../../public/images/Fallout76Map.png')
     return msg.channel.send({ files: [mapImagePath] })
   }
 }
