a94bc4446e981a6686b7aa0b07ec527520b96873
Miles <miles.b.flo@gmail.com>
2018-06-15 18:21:11 -0700

    relocating constants to folder

---
+++ src/constants/index.js
@@ -0,0 +1,4 @@
+module.exports = {
+  prefix: '$',
+  ranks: require('./ranks')
+}
---
+++ src/constants/ranks.js
@@ -0,0 +1,4 @@
+module.exports = ['Initiate', 'Apirant', 'Squire',
+  'Knight Squire', 'Knight', 'Knight Veteran', 'Knight Sergeant', 'Knight Champion', 'Knight Captain', 'Knight Commander',
+  'Scribe Squire', 'Junior Scribe', 'Scribe', 'Field Scribe', 'Venerable Scribe', 'Senior Scribe', 'Head Scribe',
+  'Paladin', 'Paladin Captain', 'Paladin Commander', 'Star Paladin', 'Elder']
