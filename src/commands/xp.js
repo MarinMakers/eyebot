3926c92cc143d1d69a7ebc1fba8b43e9cb112ac9
Miles <miles.b.flo@gmail.com>
2018-06-17 03:24:48 -0700

    Added more whitelisted ranks to xpGive

--- src/commands/xp.js
+++ src/commands/xp.js
@@ -4,7 +4,7 @@ module.exports = {
   description: 'Give XP to a user. Need permissions.',
   usage: '@<username> <#>',
   process: async (msg, argument) => {
-    if (await checkRole(msg, ['Elder', 'Star Paladin'])) {
+    if (await checkRole(msg, ['Elder', 'Star Paladin', 'Paladin', 'Head Scribe', 'Senior Scribe'])) {
       await level.give(msg, argument)
     } else {
       await console.log(`${msg.author.username} tried to use a command without privs`)
